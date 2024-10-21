b46af442c0b5c2b5028db2e121a265aeea50dccc
AnnaKRP <annakarpiuk19.1994@gmail.com>
2024-10-21 08:33:33 -0400

    Implement Back-end in Register Component

--- giftlink-frontend/src/components/Navbar/Navbar.js
+++ giftlink-frontend/src/components/Navbar/Navbar.js
@@ -1,33 +1,117 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
+import {urlConfig} from '../../config';
+import { useAppContext } from '../../context/AuthContext';
+import { useNavigate } from 'react-router-dom';
+import './RegisterPage.css';
 
-function Navbar() {
-  return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
-      <div className="container-fluid">
-        <Link className="navbar-brand" to="/">GiftApp</Link>
-        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarNav">
-          <ul className="navbar-nav">
-            <li className="nav-item">
-              <Link className="nav-link" to="/app">Home</Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/app/gifts">Gifts</Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/app/search">Search</Link> {/* Add SearchPage link */}
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/app/login">Login</Link>
-            </li>
-          </ul>
+function RegisterPage() {
+    const [firstName, setFirstName] = useState('');
+    const [lastName, setLastName] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const [showerr, setShowerr] = useState('');
+
+    const navigate = useNavigate();
+    const { setIsLoggedIn } = useAppContext();
+
+    const handleRegister = async () => {
+        const response = await fetch(`${urlConfig.backendUrl}/api/auth/register`, {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json',
+            },
+            body: JSON.stringify({
+                firstName: firstName,
+                lastName: lastName,
+                email: email,
+                password: password
+            })
+        });
+
+        const json = await response.json();
+        console.log('json data', json);
+        console.log('er', json.error);
+
+        if (json.authtoken) {
+            sessionStorage.setItem('auth-token', json.authtoken);
+            sessionStorage.setItem('name', firstName);
+            sessionStorage.setItem('email', json.email);
+            setIsLoggedIn(true);
+            navigate('/app');
+        }
+        if (json.error) {
+            setShowerr(json.error);
+        }
+    }
+
+    return (
+        <div className="container mt-5">
+            <div className="row justify-content-center">
+                <div className="col-md-6 col-lg-4">
+                    <div className="register-card p-4 border rounded">
+                        <h2 className="text-center mb-4 font-weight-bold">Register</h2>
+                        <div className="mb-3">
+                            <label htmlFor="firstName" className="form-label">FirstName</label>
+                            <input
+                                id="firstName"
+                                type="text"
+                                className="form-control"
+                                placeholder="Enter your firstName"
+                                value={firstName}
+                                onChange={(e) => setFirstName(e.target.value)}
+                            />
+                        </div>
+
+                        {/* last name */}
+
+                        <div className="mb-3">
+                            <label htmlFor="lastName" className="form-label">LastName</label>
+                            <input
+                                id="lastName"
+                                type="text"
+                                className="form-control"
+                                placeholder="Enter your lastName"
+                                value={lastName}
+                                onChange={(e) => setLastName(e.target.value)}
+                            />
+                        </div>
+
+                        {/* email  */}
+                        <div className="mb-3">
+                            <label htmlFor="email" className="form-label">Email</label>
+                            <input
+                                id="email"
+                                type="text"
+                                className="form-control"
+                                placeholder="Enter your email"
+                                value={email}
+                                onChange={(e) => setEmail(e.target.value)}
+                            />
+
+                                <div className="text-danger">{showerr}</div>
+                        </div>
+
+                        <div className="mb-4">
+                            <label htmlFor="password" className="form-label">Password</label>
+                            <input
+                                id="password"
+                                type="password"
+                                className="form-control"
+                                placeholder="Enter your password"
+                                value={password}
+                                onChange={(e) => setPassword(e.target.value)}
+                            />
+                        </div>
+                        <button className="btn btn-primary w-100 mb-3" onClick={handleRegister}>Register</button>
+                        <p className="mt-4 text-center">
+                            Already a member? <a href="/app/login" className="text-primary">Login</a>
+                        </p>
+                    </div>
+                </div>
+            </div>
         </div>
-      </div>
-    </nav>
-  );
+    );
 }
 
-export default Navbar;
+export default RegisterPage;
